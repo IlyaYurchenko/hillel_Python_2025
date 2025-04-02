ee68fe0b5c3b9139b7f3533a041b8df84feb32c7
ilyayurchenko <ilya09yurchenko@gmail.com>
2025-04-02 18:54:02 +0300

    dz 3.3

--- lesson_3/dz 3_3.py
+++ lesson_3/dz 3_3.py
@@ -1,20 +1,21 @@
-lst = [1]
+lst = []
 lst_len = len(lst)
 
 subdiv_lst = lst_len//2
 
-if lst_len % 2 == 0:
+if not lst_len:
+    lst_new = [lst, lst]
+    print(lst_new)
+
+elif not lst_len % 2:
     lst_1 = lst[:subdiv_lst]
     lst_2 = lst[subdiv_lst:]
     lst_new = [lst_1, lst_2]
     print(lst_new)
-elif lst_len % 2 != 0:
-    lst_1 = lst[:subdiv_lst+1]
-    lst_2 = lst[subdiv_lst+1:]
+else:
+    lst_1 = lst[:subdiv_lst + 1]
+    lst_2 = lst[subdiv_lst + 1:]
     lst_new = [lst_1, lst_2]
-    print(lst_new)
-elif lst_len == 0:
-    lst_new = [lst, lst]
     print(lst_new)
 
 
