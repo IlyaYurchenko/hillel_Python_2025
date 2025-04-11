accaeb8404c74a610074c0e424c315af8c0809d6
ilyayurchenko <ilya09yurchenko@gmail.com>
2025-04-11 15:21:02 +0300

    dz 4_2

--- lesson_4/dz 4_2.py
+++ lesson_4/dz 4_2.py
@@ -1,14 +1,11 @@
-
-
 lst = [0, 1, 7, 2, 4, 8]
 len_check=len(lst)
 
 if len_check > 0:
     new_lst = lst[:len_check:2]
     result = (sum(new_lst[:3])) * lst[-1]
-    print('new list =', new_lst, 'new list sum =', sum(new_lst[:3]), 'list last element =', new_lst[-1],
+    print('new list =', new_lst, 'new list sum =', sum(new_lst[:3]), 'list last element =', lst[-1],
           '\nresult =', result)
-
 else:
     print(lst)
 
