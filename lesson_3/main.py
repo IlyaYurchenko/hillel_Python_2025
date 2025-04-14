770c4480bf059ecff76769ce56871d0f232f4c6a
ilyayurchenko <ilya09yurchenko@gmail.com>
2025-04-14 15:57:22 +0300

    dz 5_2

--- lesson_3/main.py
+++ lesson_3/main.py
@@ -1,19 +1,25 @@
-n1 = int(input("enter the first digit: "))
-n2 = int(input("enter the second digit: "))
-operator = int(input(" choose operator: \n [1] + \n [2] - \n [3] * \n [4] / \n"))
-
-if operator == 1:
-    result = n1 + n2
-elif operator == 2:
-    result = n1 - n2
-elif operator == 3:
-    result = n1 * n2
-elif operator == 4:
-    if n2 != 0:
-        result = n1 / n2
+while True:
+    usr_choise = input("Ready to count? [y] - yes [n] - no \n")
+    if usr_choise == 'n':
+        break
     else:
-        print("U can't subdivide by 0")
-else:
-    print("ERROR")
+        n1 = int(input("enter the first digit: "))
+        n2 = int(input("enter the second digit: "))
+        operator = int(input(" choose operator: \n [1] + \n [2] - \n [3] * \n [4] / \n"))
+        if operator == 1:
+            result = n1 + n2
+        elif operator == 2:
+            result = n1 - n2
+        elif operator == 3:
+            result = n1 * n2
+        elif operator == 4:
+            if n2 != 0:
+                result = n1 / n2
+            else:
+                print("U can't subdivide by 0")
+        elif operator == 5:
+            break
+        else:
+            print("ERROR")
 
-print(result)
+    print(result)
