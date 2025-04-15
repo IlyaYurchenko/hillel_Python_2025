434ba1422f402cabf080fe286b9d999ca2653da9
ilyayurchenko <ilya09yurchenko@gmail.com>
2025-04-15 23:45:43 +0300

    dz 5_3 UPD

--- lesson_5/dz 5_3.py
+++ lesson_5/dz 5_3.py
@@ -1,9 +1,12 @@
+import string
 
 usr_input = str(input('Enter string: \n'))
-j = 0
+
+usr_input = ''.join(char for char in usr_input if char not in string.punctuation)
 
 usr_input = usr_input.lower() and usr_input.title()
 new_string = usr_input.replace(' ', '')
+
 
 if len(new_string) > 140:
     new_string = new_string[:139] # +1 символ хештегу
@@ -12,5 +15,6 @@ else:
     hashtag = f"#{new_string}"
 
 print(hashtag)
+
 
 
