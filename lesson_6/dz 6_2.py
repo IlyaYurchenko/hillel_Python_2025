66a65be88178053b04c48b78b091dcd352379581
ilyayurchenko <ilya09yurchenko@gmail.com>
2025-04-22 00:43:46 +0300

    dz 6_2 day UPD

--- lesson_6/dz 6_2.py
+++ lesson_6/dz 6_2.py
@@ -7,4 +7,10 @@ remaining_sec %= 3600
 minutes = remaining_sec // 60
 seconds = remaining_sec % 60
 
-print(f"{days} днів, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
+if days % 10 == 1 and days != 11:
+    day_word = "день"
+elif 2 <= days % 10 <= 4 and not (11 <= days <= 14):
+    day_word = "дні"
+else:
+    day_word = "днів"
+print(f"{days} {day_word}, {str(hours).zfill(2)}:{str(minutes).zfill(2)}:{str(seconds).zfill(2)}")
