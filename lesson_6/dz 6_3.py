f0f15ee7824bb111e7f4aff54d00f353cc90cd25
ilyayurchenko <ilya09yurchenko@gmail.com>
2025-04-21 20:32:24 +0300

    dz 6_3 upd

--- lesson_6/dz 6_3.py
+++ lesson_6/dz 6_3.py
@@ -1,9 +1,9 @@
 num = int(input("enter the digit: \n"))
 
-while num >= 10:
+while num > 10:
     digits = [int(digit) for digit in str(num)]
+    num = 1
     for digit in digits:
-        num = 1
         num *= digit
 
 print(num)
