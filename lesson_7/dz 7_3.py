34ed42140c417dbbe687a8f729179c4c20c3a66d
ilyayurchenko <ilya09yurchenko@gmail.com>
2025-04-22 13:02:12 +0300

    dz 7_1 - 7_4 annotations

--- lesson_7/dz 7_3.py
+++ lesson_7/dz 7_3.py
@@ -1,4 +1,4 @@
-def second_index(text: str, some_str: str) -> int or None:
+def second_index(text: str, some_str: str) -> int | None:
     ind = text.find(some_str)
     ind = text.find(some_str, ind + 1)
     if ind <= 0:
