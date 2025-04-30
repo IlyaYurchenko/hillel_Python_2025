8cb392030b7fd01ef10e7d14b7f5ecd0bbf2e656
ilyayurchenko <ilya09yurchenko@gmail.com>
2025-04-30 21:26:06 +0300

    dz 10_1

---
+++ lesson_10/dz 10_1.py
@@ -0,0 +1,24 @@
+def pow(x):
+    return x ** 2
+
+def some_gen(begin, end, func):
+    """
+     begin: перший елемент послідовності
+     end: кількість елементів у послідовності
+     func: функція, яка формує значення для послідовності
+    """
+    counter = 0
+    yield begin
+    while counter < end -1:
+        counter += 1
+        x = func(begin)
+        begin = x
+        yield begin
+
+
+from inspect import isgenerator
+
+gen = some_gen(2, 4, pow)
+assert isgenerator(gen) == True, 'Test1'
+assert list(gen) == [2, 4, 16, 256], 'Test2'
+print('OK')
